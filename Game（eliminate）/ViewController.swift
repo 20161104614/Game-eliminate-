6e593234c86f50073c7eb0dfb94a61493f84d6a4
20161104614 <964829629@qq.com>
2018-12-18 19:34:05 +0800

    Initial Commit

---
+++ Game（eliminate）/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game（eliminate）
+//
+//  Created by 彭皓 on 2018/12/18.
+//  Copyright © 2018年 彭皓. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
